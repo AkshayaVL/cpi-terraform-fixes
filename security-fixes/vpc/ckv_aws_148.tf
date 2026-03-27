f7c1230900cdf396385cc45002deb42c674cc453
AkshayaVL <akshayavl@outlook.com>
2026-03-27 20:41:00 +0530

    fix: fix: Default VPC vpc-02866b003811f14d1 exists — workloads should use custom VPCs with proper segmentation

--- security-fixes/vpc/ckv_aws_148.tf
+++ security-fixes/vpc/ckv_aws_148.tf
@@ -3,7 +3,7 @@
 * Quota exceeded for metric: generativelanguage.googleapis.com/generate_content_free_tier_input_token_count, limit: 0, model: gemini-2.0-flash
 * Quota exceeded for metric: generativelanguage.googleapis.com/generate_content_free_tier_requests, limit: 0, model: gemini-2.0-flash
 * Quota exceeded for metric: generativelanguage.googleapis.com/generate_content_free_tier_requests, limit: 0, model: gemini-2.0-flash
-Please retry in 18.314082431s. [links {
+Please retry in 2.462680748s. [links {
   description: "Learn more about Gemini API quotas"
   url: "https://ai.google.dev/gemini-api/docs/rate-limits"
 }
@@ -44,6 +44,6 @@ violations {
   }
 }
 , retry_delay {
-  seconds: 18
+  seconds: 2
 }
 ]
