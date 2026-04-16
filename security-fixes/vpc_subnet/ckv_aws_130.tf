ff1412912a81851306327ca2819a55db6be26b68
AkshayaVL <akshayavl@outlook.com>
2026-04-16 20:57:03 +0530

    fix: fix: Subnet subnet-079bd3fd7ff78126f auto-assigns public IP to instances on launch

--- security-fixes/vpc_subnet/ckv_aws_130.tf
+++ security-fixes/vpc_subnet/ckv_aws_130.tf
@@ -3,7 +3,7 @@
 * Quota exceeded for metric: generativelanguage.googleapis.com/generate_content_free_tier_requests, limit: 0, model: gemini-2.0-flash
 * Quota exceeded for metric: generativelanguage.googleapis.com/generate_content_free_tier_requests, limit: 0, model: gemini-2.0-flash
 * Quota exceeded for metric: generativelanguage.googleapis.com/generate_content_free_tier_input_token_count, limit: 0, model: gemini-2.0-flash
-Please retry in 16.452896353s. [links {
+Please retry in 59.521896209s. [links {
   description: "Learn more about Gemini API quotas"
   url: "https://ai.google.dev/gemini-api/docs/rate-limits"
 }
@@ -44,6 +44,6 @@ violations {
   }
 }
 , retry_delay {
-  seconds: 16
+  seconds: 59
 }
 ]
